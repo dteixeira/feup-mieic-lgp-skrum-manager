d6053341267a060bf3c90f3451602df2d4ee8148
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-05-16 23:58:10 +0100

    Added task and generic button controls.

---
+++ WPF_sKrum/GenericControlLib/ButtonControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenericControlLib
+{
+	/// <summary>
+	/// Interaction logic for ButtonControl.xaml
+	/// </summary>
+	public partial class ButtonControl : UserControl
+	{
+        private int buttonFontSize = 50;
+        private string buttonText = "BUTTON";
+
+		public ButtonControl()
+		{
+			this.InitializeComponent();
+            this.DataContext = this;
+		}
+
+        public int ButtonFontSize
+        {
+            get { return this.buttonFontSize; }
+            set { this.buttonFontSize = value; }
+        }
+
+        public string ButtonText
+        {
+            get { return this.buttonText; }
+            set { this.buttonText = value; }
+        }
+	}
+}
---
+++ WPF_sKrum/TaskBoardControlLib/TaskControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskBoardControlLib
+{
+	/// <summary>
+	/// Interaction logic for TaskControl.xaml
+	/// </summary>
+	public partial class TaskControl : UserControl
+	{
+        private string taskDescription;
+
+		public TaskControl()
+		{
+			this.InitializeComponent();
+            this.DataContext = this;
+		}
+
+        public string TaskDescription
+        {
+            get { return this.taskDescription; }
+            set { this.taskDescription = value; }
+        }
+	}
+}
