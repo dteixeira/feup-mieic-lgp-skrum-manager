03b881ddf50f866338dd52db505bb7c043611249
Ruben Aguiar <r.aguiar9080@gmail.com>
2013-05-26 11:37:39 +0100

    Drag&Drop Added to UserControl
    Drag&Drop Added to UserButtonControl. Only active on scrum master user
    to test.

--- WPF_sKrum/GenericControlLib/UserButtonControl.xaml.cs
+++ WPF_sKrum/GenericControlLib/UserButtonControl.xaml.cs
@@ -18,6 +18,13 @@ namespace GenericControlLib
     /// </summary>
     public partial class UserButtonControl : UserControl
     {
+        private Point startpoint;
+        private bool allow_drag = false;
+        private bool started_drag = false;
+        private UserButtonControl _adorner;
+        private string AdornerLayer = "dragdropadornerLayer";
+        private Canvas _adornerLayer;
+
         private int buttonFontSize = 20;
         private string userName = "Utilizador";
         private string userPhoto = @"http://178.175.139.37/images/members/21.jpg";
@@ -46,6 +53,97 @@ namespace GenericControlLib
             set { this.userPhoto = value; }
         }
 
+        public bool IsDraggable
+        {
+            get { return this.allow_drag; }
+            set { this.allow_drag = value; }
+        }
+
         public ServiceLib.DataService.Story Story { get; set; }
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            if (!allow_drag)
+                return;
+
+            try
+            {
+                startpoint = e.GetPosition(null);
+                started_drag = true;
+
+                Visual visual = e.OriginalSource as Visual;
+                Window _topWindow = (Window)SharedTypes.Utilities.FindAncestor(typeof(Window), visual);
+                _adornerLayer = (Canvas)LogicalTreeHelper.FindLogicalNode(_topWindow, AdornerLayer);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Exception in DragDropHelper: " + exc.InnerException.ToString());
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startpoint - mousePos;
+
+            if (started_drag && e.LeftButton == MouseButtonState.Pressed &&
+                    (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                startpoint = mousePos;
+                DataObject data = new DataObject("UserButtonControl", this);
+
+                // Create a placeholder to drag.
+                _adorner = this.Clone();
+                _adorner.Opacity = 0.6;
+                _adorner.IsHitTestVisible = false;
+                _adorner.MaxWidth = 150;
+                _adorner.MaxHeight = 250;
+
+                _adornerLayer.Visibility = Visibility.Visible;
+                _adornerLayer.Children.Add(_adorner);
+
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Move);
+                _adornerLayer.Children.Remove(_adorner);
+                _adornerLayer.Visibility = Visibility.Collapsed;
+                started_drag = false;
+            }
+            else if (e.LeftButton == MouseButtonState.Released)
+            {
+                started_drag = false;
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            started_drag = false;
+            e.Handled = true;
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            base.OnGiveFeedback(e);
+
+            Point mousePos = SharedTypes.Utilities.GetMousePositionWin32();
+
+            Canvas.SetLeft(_adorner, mousePos.X);
+            Canvas.SetTop(_adorner, mousePos.Y);
+
+            e.Handled = true;
+        }
+
+        public UserButtonControl Clone()
+        {
+            UserButtonControl p = new UserButtonControl();
+            p.userName = this.userName;
+            p.userPhoto = this.userPhoto;
+
+            p.Width = this.Width;
+            p.Height = this.Height;
+            return p;
+        }
     }
 }
--- WPF_sKrum/ProjectTeamManagementPageLib/ProjectTeamManagementPage.xaml.cs
+++ WPF_sKrum/ProjectTeamManagementPageLib/ProjectTeamManagementPage.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProjectTeamManagementPageLib
 {
@@ -24,6 +25,12 @@ namespace ProjectTeamManagementPageLib
     /// </summary>
     public partial class ProjectTeamManagementPage : UserControl, ITargetPage
     {
+        private float scrollValue = 0.0f;
+        private DispatcherTimer countdownTimerDelayScrollUp;
+        private DispatcherTimer countdownTimerDelayScrollDown;
+        private DispatcherTimer countdownTimerScrollUp;
+        private DispatcherTimer countdownTimerScrollDown;
+
         public ApplicationPages PageType { get; set; }
         public ApplicationController.DataModificationHandler DataChangeDelegate { get; set; }
 
@@ -34,6 +41,26 @@ namespace ProjectTeamManagementPageLib
         {
             InitializeComponent();
             this.PageType = ApplicationPages.ProjectTeamManagementPage;
+
+            // Initialize scroll up delay timer.
+            this.countdownTimerDelayScrollUp = new DispatcherTimer();
+            this.countdownTimerDelayScrollUp.Tick += new EventHandler(ScrollActionDelayUp);
+            this.countdownTimerDelayScrollUp.Interval = TimeSpan.FromSeconds(1);
+
+            // Initialize scroll down delay timer.
+            this.countdownTimerDelayScrollDown = new DispatcherTimer();
+            this.countdownTimerDelayScrollDown.Tick += new EventHandler(ScrollActionDelayDown);
+            this.countdownTimerDelayScrollDown.Interval = TimeSpan.FromSeconds(1);
+
+            // Initialize scroll up timer.
+            this.countdownTimerScrollUp = new DispatcherTimer();
+            this.countdownTimerScrollUp.Tick += new EventHandler(ScrollActionUp);
+            this.countdownTimerScrollUp.Interval = TimeSpan.FromSeconds(0.01);
+
+            // Initialize scroll down timer.
+            this.countdownTimerScrollDown = new DispatcherTimer();
+            this.countdownTimerScrollDown.Tick += new EventHandler(ScrollActionDown);
+            this.countdownTimerScrollDown.Interval = TimeSpan.FromSeconds(0.01);
 
             // Register for project change notifications.
             this.DataChangeDelegate = new ApplicationController.DataModificationHandler(this.DataChangeHandler);
@@ -54,9 +81,11 @@ namespace ProjectTeamManagementPageLib
                 Project project = ApplicationController.Instance.CurrentProject;
 
                 ServiceLib.DataService.DataServiceClient connection = new ServiceLib.DataService.DataServiceClient();
-                List<Person> team = connection.GetAllPeopleInProject(project.ProjectID);
+                List<Person> team = connection.GetAllPeople();
                 connection.Close();
 
+                for(int qweqwe=0;qweqwe<20;qweqwe++)
+                {
                 // Iterate all person on team
                 foreach (var person in team.Select((s, i) => new { Value = s, Index = i }))
                 {
@@ -71,6 +100,7 @@ namespace ProjectTeamManagementPageLib
 
                     teamCollection.Add(personControl);
                 }
+                }
                 this.Team.ItemsSource = teamCollection;
             }
             catch (Exception e)
@@ -78,6 +108,56 @@ namespace ProjectTeamManagementPageLib
                 System.Console.WriteLine(e.Message);
             }
 
+        }
+
+        private void ScrollActionDelayUp(object sender, EventArgs e)
+        {
+            this.countdownTimerScrollUp.Start();
+            this.countdownTimerDelayScrollUp.Stop();
+        }
+
+        private void ScrollActionDelayDown(object sender, EventArgs e)
+        {
+            this.countdownTimerScrollDown.Start();
+            this.countdownTimerDelayScrollDown.Stop();
+        }
+
+        private void ScrollActionUp(object sender, EventArgs e)
+        {
+            scrollValue -= 10.0f;
+            if (scrollValue < 0.0f) scrollValue = 0.0f;
+            TeamScroll.ScrollToVerticalOffset(scrollValue);
+        }
+
+        private void ScrollActionDown(object sender, EventArgs e)
+        {
+            scrollValue += 10.0f;
+            if (scrollValue > TeamScroll.ScrollableHeight) scrollValue = (float)TeamScroll.ScrollableHeight;
+            TeamScroll.ScrollToVerticalOffset(scrollValue);
+        }
+
+        private void ScrollUp_Start(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollUp.Start();
+            this.countdownTimerScrollUp.Stop();
+        }
+
+        private void ScrollDown_Start(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollDown.Start();
+            this.countdownTimerScrollDown.Stop();
+        }
+
+        private void ScrollUp_Cancel(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollUp.Stop();
+            this.countdownTimerScrollUp.Stop();
+        }
+
+        private void ScrollDown_Cancel(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollDown.Stop();
+            this.countdownTimerScrollDown.Stop();
         }
 
         public PageChange PageChangeTarget(PageChangeDirection direction)
