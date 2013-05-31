dde8d3cf11982f4094f596ce4ec73a13940bc83a
Ruben Aguiar <r.aguiar9080@gmail.com>
2013-05-31 04:31:07 +0100

    Meeting Page Improved Significantly

---
+++ WPF_sKrum/GenericControlLib/MeetingControl.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GenericControlLib
+{
+    /// <summary>
+    /// Interaction logic for MeetingControl.xaml
+    /// </summary>
+    public partial class MeetingControl : UserControl
+    {
+        private Point startpoint;
+        private bool started_drag = false;
+        private MeetingControl _adorner;
+        private string AdornerLayer = "dragdropadornerLayer";
+        private Canvas _adornerLayer;
+
+
+        private DateTime meetingDate;
+        private int meetingNumber;
+        private string meetingNotes;
+
+        public MeetingControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public DateTime MeetingDate
+        {
+            get { return this.meetingDate; }
+            set { this.meetingDate = value; }
+        }
+
+        public int MeetingNumber
+        {
+            get { return this.meetingNumber; }
+            set { this.meetingNumber = value; }
+        }
+
+        public string MeetingNotes
+        {
+            get { return this.meetingNotes; }
+            set { this.meetingNotes = value; }
+        }
+
+        public string MeetingDateText
+        {
+            get { return this.meetingDate.ToShortDateString(); }
+        }
+
+        public string MeetingName
+        {
+            get { return "REUNIÃO " + this.MeetingNumber.ToString(); }
+        }
+
+        public ServiceLib.DataService.Meeting Meeting { get; set; }
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            try
+            {
+                startpoint = e.GetPosition(null);
+                started_drag = true;
+
+                Visual visual = e.OriginalSource as Visual;
+                Window _topWindow = (Window)SharedTypes.Utilities.FindAncestor(typeof(Window), visual);
+                _adornerLayer = (Canvas)LogicalTreeHelper.FindLogicalNode(_topWindow, AdornerLayer);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Exception in DragDropHelper: " + exc.InnerException.ToString());
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startpoint - mousePos;
+
+            if (started_drag && e.LeftButton == MouseButtonState.Pressed &&
+                    (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                startpoint = mousePos;
+                DataObject data = new DataObject("MeetingControl", this);
+
+                // Create a placeholder to drag.
+                _adorner = this.Clone();
+                _adorner.Opacity = 0.6;
+                _adorner.IsHitTestVisible = false;
+                _adorner.MaxWidth = 300;
+                _adorner.MaxHeight = 150;
+
+                _adornerLayer.Visibility = Visibility.Visible;
+                _adornerLayer.Children.Add(_adorner);
+
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Move);
+                _adornerLayer.Children.Remove(_adorner);
+                _adornerLayer.Visibility = Visibility.Collapsed;
+                started_drag = false;
+            }
+            else if (e.LeftButton == MouseButtonState.Released)
+            {
+                started_drag = false;
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            started_drag = false;
+            e.Handled = true;
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            base.OnGiveFeedback(e);
+
+            Point mousePos = SharedTypes.Utilities.GetMousePositionWin32();
+
+            Canvas.SetLeft(_adorner, mousePos.X);
+            Canvas.SetTop(_adorner, mousePos.Y);
+
+            e.Handled = true;
+        }
+
+        public MeetingControl Clone()
+        {
+            MeetingControl p = new MeetingControl();
+            p.MeetingDate = this.meetingDate;
+            p.MeetingNumber = this.meetingNumber;
+            p.MeetingNotes = this.meetingNotes;
+            p.Meeting = this.Meeting;
+
+            p.Width = this.Width;
+            p.Height = this.Height;
+            return p;
+        }
+    }
+}
--- WPF_sKrum/MeetingPageLib/MeetingPage.xaml.cs
+++ WPF_sKrum/MeetingPageLib/MeetingPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using ServiceLib.NotificationService;
+﻿using GenericControlLib;
+using ServiceLib.DataService;
+using ServiceLib.NotificationService;
 using SharedTypes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MeetingPageLib
 {
@@ -21,6 +25,18 @@ namespace MeetingPageLib
     /// </summary>
     public partial class MeetingPage : UserControl, ITargetPage
     {
+        private float scrollValue = 0.0f;
+
+        private DispatcherTimer countdownTimerDelayScrollUp;
+        private DispatcherTimer countdownTimerDelayScrollDown;
+        private DispatcherTimer countdownTimerScrollUp;
+        private DispatcherTimer countdownTimerScrollDown;
+
+        private int biggestNumber = 0;
+
+
+        private ObservableCollection<MeetingControl> collection = new ObservableCollection<MeetingControl>();
+
         public ApplicationPages PageType { get; set; }
         public ApplicationController.DataModificationHandler DataChangeDelegate { get; set; }
 
@@ -28,6 +44,27 @@ namespace MeetingPageLib
         {
             InitializeComponent();
             this.PageType = ApplicationPages.MeetingPage;
+
+            // Initialize scroll up delay timer.
+            this.countdownTimerDelayScrollUp = new DispatcherTimer();
+            this.countdownTimerDelayScrollUp.Tick += new EventHandler(ScrollActionDelayUp);
+            this.countdownTimerDelayScrollUp.Interval = TimeSpan.FromSeconds(1);
+
+            // Initialize scroll down delay timer.
+            this.countdownTimerDelayScrollDown = new DispatcherTimer();
+            this.countdownTimerDelayScrollDown.Tick += new EventHandler(ScrollActionDelayDown);
+            this.countdownTimerDelayScrollDown.Interval = TimeSpan.FromSeconds(1);
+
+            // Initialize scroll up timer.
+            this.countdownTimerScrollUp = new DispatcherTimer();
+            this.countdownTimerScrollUp.Tick += new EventHandler(ScrollActionUp);
+            this.countdownTimerScrollUp.Interval = TimeSpan.FromSeconds(0.01);
+
+            // Initialize scroll down timer.
+            this.countdownTimerScrollDown = new DispatcherTimer();
+            this.countdownTimerScrollDown.Tick += new EventHandler(ScrollActionDown);
+            this.countdownTimerScrollDown.Interval = TimeSpan.FromSeconds(0.01);
+
 
             // Register for project change notifications.
             this.DataChangeDelegate = new ApplicationController.DataModificationHandler(this.DataChangeHandler);
@@ -38,7 +75,95 @@ namespace MeetingPageLib
 
         private void PopulateMeeting()
         {
-            //TODO
+            try
+            {
+                // Clears meetings.
+                collection.Clear();
+
+                // Biggest Meeting Number
+                biggestNumber = 0;
+
+                // Get meetings if possible.
+                List<Meeting> meetings = ApplicationController.Instance.CurrentProject.Meetings;
+
+                // Iterate all meetings in the project.
+                foreach (var meeting in meetings)
+                {
+                    if (meeting.Number > biggestNumber)
+                        biggestNumber = meeting.Number;
+
+                    // Create the meeting control.
+                    MeetingControl meetingControl = new MeetingControl
+                    {
+                        MeetingDate = meeting.Date,
+                        MeetingNumber = meeting.Number,
+                        MeetingNotes = meeting.Notes,
+                        Meeting = meeting
+                    };
+                    meetingControl.Width = Double.NaN;
+                    meetingControl.Height = Double.NaN;
+                    meetingControl.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                    meetingControl.SetValue(Grid.ColumnProperty, 0);
+
+                    collection.Add(meetingControl);
+                }
+                this.Meetings.ItemsSource = collection;
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+        }
+
+        private void ScrollActionDelayUp(object sender, EventArgs e)
+        {
+            this.countdownTimerScrollUp.Start();
+            this.countdownTimerDelayScrollUp.Stop();
+        }
+
+        private void ScrollActionDelayDown(object sender, EventArgs e)
+        {
+            this.countdownTimerScrollDown.Start();
+            this.countdownTimerDelayScrollDown.Stop();
+        }
+
+        private void ScrollActionUp(object sender, EventArgs e)
+        {
+            scrollValue -= 10.0f;
+            if (scrollValue < 0.0f) scrollValue = 0.0f;
+            MeetingScroll.ScrollToVerticalOffset(scrollValue);
+        }
+
+        private void ScrollActionDown(object sender, EventArgs e)
+        {
+            scrollValue += 10.0f;
+            if (scrollValue > MeetingScroll.ScrollableHeight) scrollValue = (float)MeetingScroll.ScrollableHeight;
+            MeetingScroll.ScrollToVerticalOffset(scrollValue);
+        }
+
+        private void ScrollUp_Start(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollUp.Start();
+            this.countdownTimerScrollUp.Stop();
+        }
+
+        private void ScrollDown_Start(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollDown.Start();
+            this.countdownTimerScrollDown.Stop();
+        }
+
+        private void ScrollUp_Cancel(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollUp.Stop();
+            this.countdownTimerScrollUp.Stop();
+        }
+
+        private void ScrollDown_Cancel(object sender, MouseEventArgs e)
+        {
+            this.countdownTimerDelayScrollDown.Stop();
+            this.countdownTimerScrollDown.Stop();
         }
 
         public PageChange PageChangeTarget(PageChangeDirection direction)
@@ -90,6 +215,64 @@ namespace MeetingPageLib
             {
                 System.Console.WriteLine(e.Message);
             }
+        }
+
+        private void SideMenuControl_AddClickEvent(object obj, MouseEventArgs e)
+        {
+            PopupFormControlLib.FormWindow form = new PopupFormControlLib.FormWindow();
+            PopupFormControlLib.TextAreaPage notesPage = new PopupFormControlLib.TextAreaPage { PageName = "notes", PageTitle = "Notas" };
+            form.FormPages.Add(notesPage);
+            ApplicationController.Instance.ApplicationWindow.SetWindowFade(true);
+            form.ShowDialog();
+            if (form.Success)
+            {
+                string notes = (string)form["notes"].PageValue;
+                Meeting meeting = new Meeting
+                {
+                    Notes = notes,
+                    Date = System.DateTime.Now,
+                    ProjectID = ApplicationController.Instance.CurrentProject.ProjectID,
+                    Number = biggestNumber + 1,
+                };
+                System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(AddMeeting));
+                thread.Start(meeting);
+            }
+            ApplicationController.Instance.ApplicationWindow.SetWindowFade(false);
+
+        }
+
+        public void AddMeeting(object obj)
+        {
+            DataServiceClient client = new DataServiceClient();
+            client.CreateMeeting((Meeting)obj);
+            client.Close();
+        }
+
+        private void SideMenuControl_RemoveDropEvent(object obj, DragEventArgs e)
+        {
+            var dataObj = e.Data as DataObject;
+            MeetingControl meetingControl = dataObj.GetData("MeetingControl") as MeetingControl;
+            if (meetingControl != null)
+            {
+                PopupFormControlLib.YesNoFormWindow form = new PopupFormControlLib.YesNoFormWindow();
+                form.FormTitle = "Apagar a Meeting?";
+                ApplicationController.Instance.ApplicationWindow.SetWindowFade(true);
+                form.ShowDialog();
+                if (form.Success)
+                {
+                    System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(DeleteMeeting));
+                    thread.Start(meetingControl.Meeting);
+                }
+                ApplicationController.Instance.ApplicationWindow.SetWindowFade(false);
+            }
+        }
+
+        public void DeleteMeeting(object obj)
+        {
+            Meeting meeting = (Meeting)obj;
+            DataServiceClient client = new DataServiceClient();
+            client.DeleteMeeting(meeting.MeetingID);
+            client.Close();
         }
 
     }
