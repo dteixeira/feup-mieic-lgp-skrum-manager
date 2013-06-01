4a224c24d85419a2fcb031e79e8d912f50f3f980
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-06-01 16:09:46 +0100

    Statistics layout changed.

--- WPF_sKrum/ProjectStatisticsPageLib/ProjectStatisticsPage.xaml.cs
+++ WPF_sKrum/ProjectStatisticsPageLib/ProjectStatisticsPage.xaml.cs
@@ -128,8 +128,8 @@ namespace ProjectStatisticsPageLib
             }
 
             GraphicControl graphic = new GraphicControl(data);
-            graphic.SetValue(Grid.RowProperty, 2);
-            graphic.Margin = new Thickness(40,10,40,40);
+            graphic.SetValue(Grid.RowProperty, 1);
+            graphic.Margin = new Thickness(0,0,0,0);
             this.LeftArea.Children.Add(graphic);
 
             /*GraphicColumnControl graphic2 = new GraphicColumnControl(data[1]);
